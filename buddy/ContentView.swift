f38de69118226991fe1e60ffed57356eed045d72
nuyhhyun <sarahs19991115@gmail.com>
2025-04-18 14:19:38 +0900

    Initial Commit

---
+++ buddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  buddy
+//
+//  Created by 이서현 on 4/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ buddy/buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  buddyApp.swift
+//  buddy
+//
+//  Created by 이서현 on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
